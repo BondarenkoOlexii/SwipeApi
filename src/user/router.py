9a460dc170d2b93a80649f328bcb3a9768a27576
BondarenkoOlexii <bondarenkoalosg@gmail.com>
2026-09-09 13:50:11 +0300

    registry.py

--- src/user/router.py
+++ src/user/router.py
@@ -15,25 +15,10 @@ from .services import UserService
 
 router = APIRouter(prefix="/users", tags=["User"])
 
-
-# @router.get("/", response_model=list[UserResponse])
-# @inject
-# async def get_users(service: FromDishka[UserService]):
-#     return await service.get_user()
-
-
 @router.get("/profile", response_model=UserResponse)
 @inject
 async def get_user(service: FromDishka[UserService], user_id: int):
     return await service.get_user(user_id=user_id)
-
-
-# @router.get("/{user_id}/update", response_model=UserResponse)
-# @inject
-# async def get_profile(
-#     data: ProfileUpdate, current_user: Annotated[str, Depends(token_check)]
-# ):
-#     return {"email": current_user.email, "id": current_user.id}
 
 
 @router.patch("/profile/update")
